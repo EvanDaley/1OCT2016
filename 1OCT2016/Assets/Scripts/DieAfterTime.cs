cb39290e55b1a249696648fb6183726c990a10d7
Evan <daley.evan@gmail.com>
2016-09-30 23:06:34 -0700

    Add Simple UI

---
+++ 1OCT2016/Assets/Scripts/DieAfterTime.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DieAfterTime : MonoBehaviour {
+
+	public float lifeSpan = 1f;
+	private float deathTime = float.MaxValue;
+
+	// Use this for initialization
+	void Start () {
+		deathTime = Time.time + lifeSpan;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > deathTime)
+			Destroy (gameObject);
+	}
+}
--- 1OCT2016/Assets/Scripts/Player.cs
+++ 1OCT2016/Assets/Scripts/Player.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour 
 {
 	Rigidbody2D rbody;
 	private float distToGround;
+	public float distanceToGround;
 	public LayerMask ground;
 	private Renderer render;
 
@@ -16,6 +18,18 @@ public class Player : MonoBehaviour
 	public float jumpForce = 200f;
 	public int maxJumps = 3;
 	private int jumpsLeft = 0;
+
+	public Text jumpText;
+	public Text scoreText;
+	public Text recordText;
+
+	public int score = 0;
+	public int record = 0;
+
+	public float jumpGroundCheckDelay = .2f;
+	private float jumpGroundCheckCooldown;
+
+	public float scoreCooldown = 1f;
 
 	private GameObject explosionInstance;
 
@@ -30,10 +44,27 @@ public class Player : MonoBehaviour
 		render = GetComponent<Renderer> ();
 
 		startPos = transform.position;
+
+		record = PlayerPrefs.GetInt ("record", 0);
+		recordText.text = record.ToString ();
 	}
 	
 	void Update () 
 	{
+		if (Time.time > scoreCooldown)
+		{
+			scoreCooldown = Time.time + 1f;
+			score += 1;
+			scoreText.text = score.ToString ();
+			if (score > record)
+			{
+				record = score;
+				PlayerPrefs.SetInt ("record", record);
+				recordText.text = score.ToString ();
+			}
+		}
+
+		CheckGrounded ();
 		rbody.AddForce (Vector3.right * moveForce);
 
 		if (transform.position.y < -6 && dead == false)
@@ -43,22 +74,25 @@ public class Player : MonoBehaviour
 	public void Jump()
 	{
 		print ("Trying to jump");
-		CheckGrounded ();
 
 		if(jumpsLeft > 0)
 		{
+			jumpGroundCheckCooldown = Time.time + jumpGroundCheckDelay;
 			rbody.AddForce (Vector3.up * jumpForce);
 			jumpsLeft -= 1;
+			jumpText.text = jumpsLeft.ToString ();
 		}
 	}
 
 	bool CheckGrounded()
 	{
-		bool grounded = Physics2D.OverlapArea(new Vector2(transform.position.x - .3f, transform.position.y + .1f), new Vector2(transform.position.x + .3f, transform.position.y - .3f - distToGround), ground); 
+		bool grounded = Physics2D.OverlapArea(new Vector2(transform.position.x - .3f, transform.position.y + .1f), new Vector2(transform.position.x + .3f, transform.position.y - distanceToGround - distToGround), ground); 
 
-		if (grounded)
+		if (grounded && jumpGroundCheckCooldown < Time.time)
+		{
 			jumpsLeft = maxJumps;
-		
+			jumpText.text = jumpsLeft.ToString ();
+		}
 		return grounded;
 	}
 
@@ -88,10 +122,12 @@ public class Player : MonoBehaviour
 	void Respawn()
 	{
 		rbody.velocity = Vector3.zero;
-
 		dead = false;
 		render.enabled = true;
 		transform.position = startPos;
 		Destroy (explosionInstance);
+
+		score = 0;
+		scoreText.text = score.ToString ();
 	}
 }
